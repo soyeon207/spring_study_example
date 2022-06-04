9d8b096884ce4bcac0a60c65b7e82747ec8f7000
soyeon207 <psy010207@naver.com>
2022-06-04 22:54:04 +0900

    Feat: 05 인터셉터 (#1)

--- spring-mvc-exception/src/main/java/example/exception/WebConfig.java
+++ spring-mvc-exception/src/main/java/example/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package example.exception;
 
 import example.exception.filter.LogFilter;
+import example.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +14,15 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류 페이지 경로
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<Filter>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
